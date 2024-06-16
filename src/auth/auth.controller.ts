fb907ba4c65c2571f79b71d24ae15bed09136e43
rakotomalala herifanantenana <herifanantenana17@gmail.com>
2024-06-16 16:22:49 +0300

    create signup dto and validation

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { SignupDto } from './dtos/signup.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  //TODO: Post Signup
+  @Post('signup') //auth/signup
+  async signUp(@Body() signupData: SignupDto): Promise<any> {
+    // Call the service
+    console.log(signupData);
+  }
+
+  //TODO: Post Login
+
+  //TODO: Post Refresh Token
 }
---
+++ src/auth/dtos/signup.dto.ts
@@ -0,0 +1,17 @@
+import { IsEmail, IsString, MinLength, Matches } from 'class-validator';
+
+export class SignupDto {
+  @IsString()
+  name: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @MinLength(6)
+  @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{6,}$/, {
+    message:
+      'Password must contain at least one uppercase letter, one lowercase letter and one number',
+  })
+  password: string;
+}
