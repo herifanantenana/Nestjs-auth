da2b0be8157c66bbfef5d201f23cbb9995e0e506
rakotomalala herifanantenana <herifanantenana17@gmail.com>
2024-06-16 16:38:03 +0300

    config connextion mongoose and inject model in service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { SignupDto } from './dtos/signup.dto';
+import { User } from './schemas/user.schema';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(@InjectModel(User.name) private UserModel: Model<User>) {}
+
+  async signup(signupDate: SignupDto) {
+    const emailIsUse = await this.UserModel.findOne({
+      email: signupDate.email,
+    });
+    if (emailIsUse) {
+      throw new BadRequestException('Email is already in use');
+    }
+    console.log(signupDate);
+  }
+}
